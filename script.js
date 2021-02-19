701f74fa18a1f853e9de3aa7327dc476ab9e0c39
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-19 12:47:39 +0100

    Learning Remote Data

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
 // REMOTE DATA CHART //
+
+// INSERT REMOTE DATA CANVAS //
 var remoteCanvas = document.createElement("canvas");
 remoteCanvas.id = "remoteChart";
 var remoteTarget = document.getElementsByTagName("h1");
 remoteTarget[0].appendChild(remoteCanvas);
+
+
+// GET REMOTE DATA //
+var labels;
+var values;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var json = JSON.parse(this.response);
+        // Map json labels  back to values array
+        labels = json.map(function (e) {
+            return e.person.name;
+        });
+        // Map json values back to values array
+        values = json.map(function (e) {
+            return (e.finalWorth / 1000); // Divide to billions in units of ten
+        });
+    }
+};
+xhttp.open("GET", "https://forbes400.herokuapp.com/api/forbes400?limit=10", false);
+xhttp.send();
+
+// CREATE REMOTE CHART //
+
 var remoteContainer = document.getElementById("remoteChart");
 var remoteChart = new Chart(remoteContainer, {
-    type: 'bar',
+    type: 'horizontalBar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: labels,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+                label: "Title",
+                data: values,
+                backgroundColor: ['rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                    'rgb(54, 162, 235)',
+                ],
+        }],
     }
 });
 
